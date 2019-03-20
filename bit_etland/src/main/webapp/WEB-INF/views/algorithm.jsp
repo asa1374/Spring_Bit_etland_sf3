0821686da442169256fc93cb3cb13c0f36f6e8c3
1027 <1027@hb1011>
2019-03-20 17:24:10 +0900

    2019.03.20 fainal

--- bit_etland/src/main/webapp/WEB-INF/views/algorithm.jsp
+++ bit_etland/src/main/webapp/WEB-INF/views/algorithm.jsp
@@ -64,33 +64,30 @@
       </div>
     </div>
 
-    <div class="col-sm-9">
-      <h4><small>RECENT POSTS</small></h4>
+    <div id="dugi_cont" class="col-sm-9">
+      <h4 id="dugi_start1"><small>RECENT POSTS</small></h4>
       <hr>
       <h2 id="ex1">I Love Food</h2>
       <h5><span class="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>
-      <h5><span id="res_btn_1" class="label label-danger">Food</span> <span id="reset_btn_1" class="label label-primary">Ipsum</span></h5><br>
+      <h5 id="h5"><span id="res_btn_1" class="label label-danger">Food</span> <span id="reset_btn_1" class="label label-primary">Ipsum</span></h5><br>
       <p id="res_1">Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <br><br>
+      <br id="dugi_end1"><br>
       
-      <h4><small>RECENT POSTS</small></h4>
+      <h4 id="dugi_start2"><small>RECENT POSTS</small></h4>
       <hr>
       <h2 id="ex2">Officially Blogging</h2>
       <h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
       <h5><span id="res_btn_2" class="label label-success">Lorem</span></h5><br>
       <p id="res_2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <hr>
-
+      <hr id="dugi_end2">
 	
       <h4>Leave a Comment:</h4>
-      <form role="form">
-        <div class="form-group">
-          <input id="start" type="text" />시작
-          <input id="end" type="text" />끝
-          <input id="diff" type="text" />공차
-        </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </form>
+	      <form role="form">
+	        <div class="form-group">
+	          <textarea class="form-control" rows="3" required></textarea>
+	        </div>
+	        <button type="submit" class="btn btn-success">Submit</button>
+	      </form>
       <br><br>
       
       <p><span class="badge">2</span> Comments:</p><br>
--- bit_etland/src/main/webapp/resources/js/algorithm.js
+++ bit_etland/src/main/webapp/resources/js/algorithm.js
@@ -10,14 +10,99 @@ algo = (()=>{
 	let setContentView = ()=>{
 		$.getScript($.js()+'/compo.js',()=>{
 			//$('body').html($$.div({id:'test',clazz:'clazz'}))
-			$('#nav').children().eq(0).html($$.a({url:'#',id:'su'}).text('수열'));
-			$('#nav').children().eq(1).html($$.a({url:'#',id:'ma'}).text('수학'));
-			$('#nav').children().eq(2).html($$.a({url:'#',id:'ar'}).text('배열'));
-			$('#nav').children().eq(3).html($$.a({url:'#',id:'coll'}).text('자료구조'));
-			$('#nav').append($$.li({id:''})).children().eq(4).html($$.a({url:'#',id:'ap'}).text('응용'));
-			$('#ex1').text('수열 1 - 등차 수열의 합계');
-			$('#ex2').text('수열 2 - 등비 수열의 합계');
-			$('#res_1')+inputform();
+			dugi_nav();
+			dugi_rm_ex2();
+			dugi_exam('등차');
+			dugi_input_ex1_itter();
+			var str = $('#newdiv1').html();
+			$('#newdiv1').remove();
+			let arr = [
+				{ id:'1', val :'등차'},
+				{ id:'2', val :'등비'},
+				{ id:'3', val :'팩토리얼'},
+				{ id:'4', val :'피보나치'}
+				];
+			$.each(arr,(i,j)=>{
+				$('#dugi_start1').append(str);
+				$('#ex1').attr('id','ex_'+j.id);
+				$('#ex_'+j.id).text(j.val);
+				
+			});
+		})
+	};
+	let dugi_nav =()=>{
+		$('#nav').children().eq(0).html($$.a({url:'#',id:'su'}).text('수열'));
+		$('#nav').children().eq(1).html($$.a({url:'#',id:'ma'}).text('수학'));
+		$('#nav').children().eq(2).html($$.a({url:'#',id:'ar'}).text('배열'));
+		$('#nav').children().eq(3).html($$.a({url:'#',id:'coll'}).text('자료구조'));
+		$('#nav').append($$.li({id:''})).children().eq(4).html($$.a({url:'#',id:'ap'}).text('응용'));
+	};
+	
+	let aa =()=>{
+		$('#dugi_cont').prepend(()=>{
+			dugi_exam();
+			dugi_rm_ex2();
+		});
+	};
+	let dugi_input = x=>{
+		$('#ex1').text(x);
+		$('#res_1').html($$.form()
+		.append($$.div({id:'st'})
+		.append($$.input({type:'text', id:'start'}))
+		.append($$.label({name:'start'}).text('시작값'))).append($$.br())
+		.append($$.div({id:'en'})
+		.append($$.input({type:'text' ,id:'end'}))
+		.append($$.label({name:'end'}).text('마지막'))).append($$.br())
+		.append($$.div({id:'di'})
+		.append($$.input({type:'text', id:'diff'}))
+		.append($$.label({name:'diff'}).text('공차'))));
+	};
+	
+	let dugi_input1 = x=>{
+		$('#ex1').text(x);
+		$('#right_top').prepend($$.form()
+		.append($$.div({id:'st'})
+		.append($$.input({type:'text', id:'start'}))
+		.append($$.label({name:'start'}).text('시작값'))).append($$.br())
+		.append($$.div({id:'en'})
+		.append($$.input({type:'text' ,id:'end'}))
+		.append($$.label({name:'end'}).text('마지막'))).append($$.br())
+		.append($$.div({id:'di'})
+		.append($$.input({type:'text', id:'diff'}))
+		.append($$.label({name:'diff'}).text('공차'))));
+	};
+	let dugi_rm_ex1 = () =>{
+		$('#ex1').before('<div id="rm_start1">');
+		$('#dugi_end1').after('<div id="rm_end1">');
+		$('#rm_start1').nextUntil('#rm_end1').remove();
+	};
+	
+	let dugi_input_ex1_itter = ()=>{
+		$('#ex1').before('<div id="rm_start1">');
+		$('#dugi_end1').after('<div id="rm_end1">');
+		$('#rm_start1').nextUntil('#rm_end1').wrapAll('<div id="newdiv1"></div>');
+	};
+	let dugi_rm_ex2 = () =>{
+		$('#dugi_start2').before('<div id="rm_start2">');
+		$('#dugi_end2').after('<div id="rm_end2">');
+		$('#rm_start2').nextUntil('#rm_end2').remove();
+	};
+	let dugi_reset = ()=>{
+		$('#reset_btn_1').text('리셋').addClass('cursor').click(()=>{
+			$('#res_1').html($$.form()
+					.append($$.div({id:'st'})
+					.append($$.input({type:'text', id:'start'}))
+					.append($$.label({name:'start'}).text('시작값'))).append($$.br())
+					.append($$.div({id:'en'})
+					.append($$.input({type:'text', id:'end'}))
+					.append($$.label({name:'end'}).text('마지막'))).append($$.br())
+					.append($$.div({id:'di'})
+					.append($$.input({type:'text', id:'diff'}))
+					.append($$.label({name:'diff'}).text('공차'))));
+		});
+	};
+	let dugi_exam = x=>{ 
+			dugi_input(x);
 			$('#res_btn_1').text('결과').addClass('cursor').click(()=>{
 				$.ajax({
 					url : $.ctx()+'/algo/seq/1',
@@ -31,36 +116,39 @@ algo = (()=>{
 					contentType : "application/json",
 					success : d=>{
 						alert('넘어온 값'+d.result);
-						$('#res_1').html($$.h({s:'2'}).text('결과값 :'+d.result));
+						$(this).html($$.h({s:'2'}).text('결과값 :'+d.result));
 					},
 					error : e=>{
 						alert('숫자 똑바로 입력해 김태혁이');
 					}
 				});
 			});
-			$('#reset_btn_1').text('리셋').addClass('cursor').click(()=>{
-				$('#res_1')+inputform();
+			dugi_reset();
+	};
+	let dugi_exam1 = x=>{ 
+		dugi_input1(x);
+		$('#res_btn_1').text('결과').addClass('cursor').click(()=>{
+			$.ajax({
+				url : $.ctx()+'/algo/seq/1',
+				type : 'post',
+				data : JSON.stringify({
+					start : $('#start').val(),
+					end : $('#end').val(),
+					diff : $('#diff').val()
+				}),
+				dataType : 'json',
+				contentType : "application/json",
+				success : d=>{
+					alert('넘어온 값'+d.result);
+					$('#res_1').html($$.h({s:'2'}).text('결과값 :'+d.result));
+				},
+				error : e=>{
+					alert('숫자 똑바로 입력해 김태혁이');
+				}
 			});
-			ex2();
-		})
-	};
-	let ex2 = ()=>{
-		$('#res_2')+inputform();
-		$('#di').children().eq(1).html($$.label({name:'div'}).text('등비'));
-		
-	};
-	let inputform = ()=>{
-		return '.html($$.form()'+
-				'.append($$.div({id:'+'st'+'})'+
-				'.append($$.input({type:'+'text',+'id:'+'start'+'}))'+
-				'.append($$.label({name:'+'start'+'}).text('+'시작값'+'))).append($$.br())'+
-				'.append($$.div({id:'+'en'+'})'+
-				'.append($$.input({type:'+'text'+',id:'+'end'+'}))'+
-				'.append($$.label({name:'+'end'+'}).text('+'마지막'+'))).append($$.br())'+
-				'.append($$.div({id:'+'di'+'})'+
-				'.append($$.input({type:'+'text','id:'+'diff'+'}))'+
-				'.append($$.label({name:'+'diff'+'}).text('+'공차'+'))))'
-	};
+		});
+		dugi_reset();
+};
 	return {init:init,
 		onCreate:onCreate};
 })();
